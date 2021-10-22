2e0010c9b9f59416e588c1a095d9a59bc188f146
heredia05 <62081755+heredia05@users.noreply.github.com>
2021-10-21 22:49:30 -0500

    Add files via upload

--- src/main/java/co/activityschool/controller/RolController.java
+++ src/main/java/co/activityschool/controller/RolController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.activityschool.dto.RolCrearRequest;
 import co.activityschool.dto.RolResponse;
 import co.activityschool.service.RolService;
 import co.activityschool.util.EntidadToConverter;
@@ -28,5 +31,10 @@ public class RolController {
 	@GetMapping(value = "roles/{id}")
 	private RolResponse obtenerRolPorId(@PathVariable Long id) {
 		return entidadToConverter.convertirRol(rolService.obtenerRolPorId(id));
+	}
+	
+	@PostMapping(value = "rol")
+	private RolResponse crearRol(@RequestBody RolCrearRequest rolCrearRequest) {
+		return entidadToConverter.convertirRol(rolService.crearRol(rolCrearRequest));
 	}
 }
--- src/main/java/co/activityschool/service/RolService.java
+++ src/main/java/co/activityschool/service/RolService.java
@@ -1,10 +1,12 @@
 package co.activityschool.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.activityschool.dto.RolCrearRequest;
 import co.activityschool.entities.Rol;
 import co.activityschool.repository.RolRepository;
 
@@ -20,5 +22,14 @@ public class RolService {
 	
 	public Rol obtenerRolPorId(Long id) {
 		return rolRepository.findById(id).get();
+	}
+	
+	public Rol crearRol(RolCrearRequest rolCrearRequest) {
+		Rol rol = new Rol();
+		rol.setEstado(true);
+		rol.setFechaCreacion(new Date());
+		rol.setRol(rolCrearRequest.getRol());
+		return rolRepository.save(rol);
+		
 	}
 }
