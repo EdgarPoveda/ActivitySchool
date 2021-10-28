0961f0ad8aa18596055f7d61a68f62df279619b2
heredia05 <62081755+heredia05@users.noreply.github.com>
2021-10-27 21:41:49 -0500

    Add files via upload

--- src/main/java/co/activityschool/controller/UsuarioController.java
+++ src/main/java/co/activityschool/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package co.activityschool.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,28 +25,57 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private EntidadToConverter entidadToConverter;
-	
+
 	@GetMapping(value = "usuarios")
-	private List<UsuarioResponse> obtenerUsuarios(){
-		return entidadToConverter.convertirUsuarios(usuarioService.obtenerUsuarios());
+	private ResponseEntity<?> obtenerUsuarios() {
+		try {
+			return new ResponseEntity<>(entidadToConverter.convertirUsuarios(usuarioService.obtenerUsuarios()),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
 	@GetMapping(value = "usuarios/{id}")
-	private UsuarioResponse obtenerUsuarioPorID(@PathVariable Long id) {
-		return entidadToConverter.convertirUsuario(usuarioService.obtenerUsusarioPorId(id));
+	private ResponseEntity<?> obtenerUsuarioPorID(@PathVariable Long id) {
+		try {
+			return new ResponseEntity<>(entidadToConverter.convertirUsuario(usuarioService.obtenerUsusarioPorId(id)),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
 	@PostMapping(value = "login")
-	private UsuarioResponse login(@RequestBody UsuarioLogin usuarioLogin) {
-		return entidadToConverter.convertirUsuario(usuarioService.login(usuarioLogin));
+	private ResponseEntity<?> login(@RequestBody UsuarioLogin usuarioLogin) {
+		Map response = new HashMap();
+		try {
+			response.put("token", usuarioService.login(usuarioLogin));
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (Exception ex) {
+			response.put("error", ex.getMessage());
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
 	@PostMapping(value = "usuarios")
 	private UsuarioResponse crearUsuario(@RequestBody UsuarioCrearRequest usuarioCrearRequest) {
 		return entidadToConverter.convertirUsuario(usuarioService.crearUsuario(usuarioCrearRequest));
+	}
+
+	@GetMapping(value = "usuario/{token}")
+	private ResponseEntity<?> usuarioPorToken(@PathVariable String token) {
+		Map response = new HashMap();
+		try {
+			response.put("token", entidadToConverter.convertirUsuario(usuarioService.usuarioPorIdAutomatico(token)));
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (Exception ex) {
+			response.put("error", ex.getMessage());
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
